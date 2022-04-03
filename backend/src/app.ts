48f94a06bcf4790bec057144b265136fcd37febf
Tiago Gomes <up201806658@fe.up.pt>
2022-04-03 21:05:38 +0100

    Add: eslint and prettier setup

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,13 +2,13 @@ import express from "express"
 import dotenv from "dotenv"
 import routes from "./routes"
 
-dotenv.config();
+dotenv.config()
 
 const app = express()
 const port = process.env.PORT
 
 app.listen(port, () => {
-  console.log(`Application running in port ${port}.`)
+    console.log(`Application running in port ${port}.`)
 })
 
-app.use('/hello', routes.hello)
+app.use("/hello", routes.hello)
--- backend/src/controller/hello.controller.ts
+++ backend/src/controller/hello.controller.ts
@@ -1,9 +1,9 @@
 import { Request, Response } from "express"
 
 async function get(req: Request, res: Response) {
-  res.send("Hello World!")
+    res.send("Hello World!")
 }
 
 export default {
-  get,
+    get
 }
