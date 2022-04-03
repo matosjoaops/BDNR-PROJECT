70c041d3c3e92246cc7e1361c2c6f6a5f4e5a15d
Tiago Gomes <up201806658@fe.up.pt>
2022-04-03 20:25:59 +0100

    Add: docker and express setup

---
+++ backend/src/app.ts
@@ -0,0 +1,14 @@
+import express from "express"
+import dotenv from "dotenv"
+import routes from "./routes"
+
+dotenv.config();
+
+const app = express()
+const port = process.env.PORT
+
+app.listen(port, () => {
+  console.log(`Application running in port ${port}.`)
+})
+
+app.use('/hello', routes.hello)
