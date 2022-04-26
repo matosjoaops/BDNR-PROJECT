608922035ec4391d0f58cdf1f305666e41c1d355
Tiago Gomes <up201806658@fe.up.pt>
2022-04-26 16:14:05 +0100

    Fix: import data to couchbase script

--- scripts/import_data.sh
+++ scripts/import_data.sh
@@ -6,9 +6,8 @@ echo
 echo "Creating cluster for Couchbase..."
 echo
 
-couchbase-cli cluster-init -c http://localhost:8091 --cluster-username Administrator \
- --cluster-password bdnr-1234 --services data --cluster-ramsize 2048 \
- --cluster-port 5000
+couchbase-cli cluster-init -c http://0.0.0.0:8091 --cluster-name bdnr-project --cluster-username Administrator \
+--cluster-password bdnr-12345 --services data --cluster-ramsize 2048
 
 
 echo
@@ -20,16 +19,16 @@ echo "Creating bucket for posts..."
 echo
 
 # Posts: Create bucket and import documents.
-couchbase-cli bucket-create -c http://localhost:8091 --username Administrator \
- --password bdnr-12345 --bucket posts --bucket-type couchbase \
- --bucket-ramsize 1024
+couchbase-cli bucket-create -c http://0.0.0.0:8091 --username Administrator \
+--password bdnr-12345 --bucket posts --bucket-type couchbase \
+--bucket-ramsize 1024
 
 
 echo
 echo "Importing posts..."
 echo
 
-cbimport json --d file:///posts.json --c http://localhost:8091 --u Administrator --p bdnr-12345 --b posts  --f list --g post::#MONO_INCR#
+cbimport json -d file:///posts.json -c http://0.0.0.0:8091 -u Administrator -p bdnr-12345 -b posts  -f list -g post::#MONO_INCR#
 
 
 echo
@@ -37,14 +36,14 @@ echo "Creating bucket for users..."
 echo
 
 # Users: Create bucket and import documents.
-couchbase-cli bucket-create -c http://localhost:8091 --username Administrator \
- --password bdnr-12345 --bucket users --bucket-type couchbase \
- --bucket-ramsize 1024
+couchbase-cli bucket-create -c http://0.0.0.0:8091 --username Administrator \
+--password bdnr-12345 --bucket users --bucket-type couchbase \
+--bucket-ramsize 1024
 
 
 echo
 echo "Importing users..."
 echo
 
-cbimport json --d file:///users.json --c http://localhost:8091 --u Administrator --p bdnr-12345 --b users  --f list --g %id%
+cbimport json -d file:///users.json -c http://0.0.0.0:8091 -u Administrator -p bdnr-12345 -b users  -f list -g %id%
 
