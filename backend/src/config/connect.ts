a123ab6e353d8bc24ec0b57285310a91a1e4c8c0
Tiago Gomes <up201806658@fe.up.pt>
2022-05-17 17:40:39 +0100

    Add: couchbase connection funcion

---
+++ backend/src/config/connect.ts
@@ -0,0 +1,28 @@
+import { Cluster, connect } from "couchbase"
+
+async function connectToCluster(): Promise<Cluster> {
+    try {
+        console.log("Connecting......")
+        const cluster: Cluster = await connect("couchbase://couchbase", {
+            username: "Administrator",
+            password: "bdnr-12345",
+            timeouts: {
+                kvTimeout: 10000,
+                kvDurableTimeout: 10000,
+                viewTimeout: 10000,
+                queryTimeout: 10000,
+                analyticsTimeout: 10000,
+                searchTimeout: 10000,
+                managementTimeout: 10000
+            }
+        })
+        console.log("Connected......")
+
+        return cluster
+    } catch (error) {
+        console.error("Error connecting to couchbase!")
+        throw new Error("Error connecting to couchbase!")
+    }
+}
+
+export default connectToCluster
--- backend/src/controller/user.controller.ts
+++ backend/src/controller/user.controller.ts
@@ -1,30 +1,15 @@
 import { Request, Response } from "express"
-import { Bucket, Cluster, Collection, connect, GetResult, MutationResult } from "couchbase"
+import { Cluster } from "couchbase"
+
+import connectToCluster from "../config/connect"
 
 async function get(req: Request, res: Response) {
     try {
-        console.log("Connecting......")
-        const cluster: Cluster = await connect("couchbase://couchbase", {
-            username: "Administrator",
-            password: "bdnr-12345",
-            timeouts: {
-                kvTimeout: 10000,
-                kvDurableTimeout: 10000,
-                viewTimeout: 10000,
-                queryTimeout: 10000,
-                analyticsTimeout: 10000,
-                searchTimeout: 10000,
-                managementTimeout: 10000
-            }
-        })
-        console.log("Connected......")
+        const cluster: Cluster = await connectToCluster()
 
         const queryResult = await cluster.query("SELECT * FROM users WHERE id=$1", {
             parameters: ["009011c5-c27c-4ad1-b803-fd4c157866b4"]
         })
-        // queryResult.rows.forEach((row) => {
-        //     console.log(row)
-        // })
 
         res.status(200).json(queryResult)
     } catch (error) {
