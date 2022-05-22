837ec4e459a1eebd9ea9e2a0b92543e90d821293
Tiago Gomes <up201806658@fe.up.pt>
2022-05-23 00:30:42 +0100

    Add: get user posts

--- backend/src/routes/post.ts
+++ backend/src/routes/post.ts
@@ -3,10 +3,10 @@ import controller from "../controller/post.controller"
 
 const router = express.Router()
 
+router.get("/ratio", controller.getRatio)
 router.get("/:id", controller.get)
 router.post("/", controller.post)
 router.put("/:id", controller.put)
 router.delete("/:id", controller.delete)
-router.get("/ratio", controller.getRatio)
 
 export default router
