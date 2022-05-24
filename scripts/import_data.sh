662b1eea3be297fb3820b640d042af51aa229b32
@david.regatia1 <david.regatia@kodly.io>
2022-05-24 20:46:34 +0100

    Adds curl to script in order to create index for fts

--- scripts/import_data.sh
+++ scripts/import_data.sh
@@ -73,6 +73,15 @@ cbq -u Administrator -p bdnr-12345 -e "http://0.0.0.0:8091" --script="CREATE IND
 cbq -u Administrator -p bdnr-12345 -e "http://0.0.0.0:8091" --script="CREATE INDEX id_index ON \`users\`(id);"
 cbq -u Administrator -p bdnr-12345 -e "http://0.0.0.0:8091" --script="CREATE INDEX username_index ON \`users\`(username);"
 
+
+sleep 5
+
+echo
+echo "Creating Full-Text searc index for POSTS"
+echo
+
+curl 'http://localhost:8091/_p/fts/api/index/posts-index' -X PUT -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:100.0) Gecko/20100101 Firefox/100.0' -H 'Accept: application/json, text/plain, */*' -H 'Accept-Language: pt-PT,pt;q=0.8,en;q=0.5,en-US;q=0.3' -H 'Accept-Encoding: gzip, deflate, br' -H 'invalid-auth-response: on' -H 'Cache-Control: no-cache' -H 'Pragma: no-cache' -H 'ns-server-ui: yes' -H 'Content-Type: application/json;charset=utf-8' -H 'Origin: http://localhost:8091' -H 'Connection: keep-alive' -H 'Referer: http://localhost:8091/ui/index.html' -H 'Cookie: ui-auth-localhost%3A8091=520974c0874e8a029b5b61df049456c8' -H 'Sec-Fetch-Dest: empty' -H 'Sec-Fetch-Mode: cors' -H 'Sec-Fetch-Site: same-origin' --data-raw '{"name":"posts-index","type":"fulltext-index","params":{"mapping":{"default_mapping":{"enabled":true,"dynamic":true},"default_type":"_default","default_analyzer":"standard","default_datetime_parser":"dateTimeOptional","default_field":"_all","store_dynamic":false,"index_dynamic":true,"docvalues_dynamic":false},"store":{"indexType":"scorch","kvStoreName":""},"doc_config":{"mode":"type_field","type_field":"type"}},"sourceType":"couchbase","sourceName":"posts","sourceUUID":"f4b41d0d7474f58cbb836192cf68a290","sourceParams":{},"planParams":{"maxPartitionsPerPIndex":1024,"numReplicas":0,"indexPartitions":1},"uuid":""}'
+
 echo
 echo "Done!"
 echo
